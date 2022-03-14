1a986d4008dafa36b0d0f6735130d85ab7c0d59d
ryoji47 <99850921+ryoji47@users.noreply.github.com>
2022-03-14 20:30:08 +0900

    ActiveStorageとnew関係の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
     
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :explanation, :category_id, :sales_status_id, :postage_id, :prefecture_id, :days_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :explanation
+    validates :category_id
+    validates :sales_status_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :days_id
+    validates :price
+  end
 end
