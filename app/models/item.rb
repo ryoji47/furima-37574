a4200c877598365942a8308a11a49025951c4d3a
ryoji47 <99850921+ryoji47@users.noreply.github.com>
2022-03-15 13:02:35 +0900

    ActiveHashによる機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
+
   belongs_to :user
   has_one_attached :image
 
@@ -10,7 +17,15 @@ class Item < ApplicationRecord
     validates :sales_status_id
     validates :postage_id
     validates :prefecture_id
-    validates :days_id
+    validates :scheduled_delivery_id
     validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: "Please choose"} do
+    validates :category_id
+    validates :sales_status_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2022_03_14_110711) do
     t.integer "sales_status_id", null: false
     t.integer "postage_id", null: false
     t.integer "prefecture_id", null: false
-    t.integer "days_id", null: false
+    t.integer "scheduled_delivery_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
