75f93db4cc1784d8b0e485dd251371a313c15bce
ryoji47 <99850921+ryoji47@users.noreply.github.com>
2022-03-16 14:26:11 +0900

    商品一覧表示機能の実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Item, type: :model do
       it 'ユーザーが紐付いていなければ出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
       it '画像が空では出品できない' do
         @item.image = nil
