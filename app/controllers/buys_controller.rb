aa23f1f66852e1a268946eb63debd4e78390a5e7
ryoji47 <99850921+ryoji47@users.noreply.github.com>
2022-03-21 12:43:18 +0900

    クレジット購入機能実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -10,6 +10,7 @@ class BuysController < ApplicationController
   def create
     @buy_order = BuyOrder.new(buy_params)
     if @buy_order.valid?
+      pay_item
       @buy_order.save
       redirect_to root_path
     else
@@ -20,7 +21,7 @@ class BuysController < ApplicationController
   private
 
   def buy_params
-    params.require(:buy_order).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buy_order).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def item_post
@@ -33,5 +34,14 @@ class BuysController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_474e2ee4127ca83f3de8d1a9"  
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buy_params[:token],    
+      currency: 'jpy'                 
+    )
   end
 end
--- app/models/buy_order.rb
+++ app/models/buy_order.rb
@@ -1,6 +1,6 @@
 class BuyOrder
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,6 +10,7 @@ class BuyOrder
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
