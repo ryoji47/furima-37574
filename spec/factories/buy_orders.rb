94e72b3ebe1b6e1132f0cfa0d603a426aa8e318f
ryoji47 <99850921+ryoji47@users.noreply.github.com>
2022-03-21 15:59:08 +0900

    テストコード

---
+++ spec/factories/buy_orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :buy_order do
+    token {"tok_abcdefghijk00000000000000000"}
+    post_code {"777-7777"}
+    prefecture_id { Faker::Number.within(range: 1..47) }
+    city { Gimei.address.city.kanji }
+    address {Gimei.address.town.kanji }
+    phone_number {"08098987878"}
+  end
+end
